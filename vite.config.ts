5ddfeb4376d1e33c75cc1aa7c571423d1fda148d
wilmer <est.wzelayam@uml.edu.ni>
2023-08-05 11:30:05 -0600

    ajustes para despliegues

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-app-web-fundamentos/',
+  base: '/encuentro-10/',
   root: "./",
   build: {
     outDir: "docs",
