de56d90f2345d420e96aaa687015c7c7812b2ccc
wilmer <est.wzelayam@uml.edu.ni>
2023-08-05 12:35:11 -0600

    first commit

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,6 +1,12 @@
-import { Title } from '../components/Title';
+import { useEffect } from "react";
+import { Title } from "../components/Title";
 
 export const Inicio = () => {
+  useEffect(() => {
+    void fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
   return (
     <>
       <Title titulo="Bienvenido!" />
