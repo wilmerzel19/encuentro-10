8da69cdfcd2725bb49cf350d8fa9839e92a3b74d
wilmer <est.wzelayam@uml.edu.ni>
2023-08-06 22:04:58 -0600

    ajustes para despliegues

--- src/components/Imagen.tsx
+++ src/components/Imagen.tsx
@@ -3,7 +3,7 @@
 export const Imagen = () => {
   return (
     <div>
-        <img src="img/paris.png" alt="paris" className="w-full max-h-80 row-span-2" />
+        <img src="public/img/paris.png" alt="paris" className="w-full max-h-80 row-span-2" />
     </div>
   )
 }
