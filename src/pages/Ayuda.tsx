7419b7b71a9a73cfcbf6b85f90ea904b0fe38a00
wilmer <est.wzelayam@uml.edu.ni>
2023-08-10 22:13:40 -0600

    ajustes para despliegues

--- src/pages/Ayuda.tsx
+++ src/pages/Ayuda.tsx
@@ -1,10 +1,15 @@
 
+import { Card } from '../components/Card'
 import { Title } from '../components/Title'
 
 export const Ayuda = () => {
   return (
     <>
       <Title titulo="Transportes" />
+
+      <br />
+      <Card  titulo="Transporte" price="$20" img={{ src: "img/transporte.png", alt: "paris" }} />
+      <br />
 
     </>
   )
--- src/pages/Sedes.tsx
+++ src/pages/Sedes.tsx
@@ -1,9 +1,14 @@
+import { Card } from '../components/Card'
 import { Title } from '../components/Title'
 
 export const Sedes = () => {
   return (
     <>
       <Title titulo="Restaurantes" />
+      <br />
+      <Card  titulo="Restaurante" price="$50" img={{ src: "img/restaurante.png", alt: "paris" }} />
+      <br />
+
     </>
   )
 }
