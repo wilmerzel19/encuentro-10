4e8180ec58052d22f17814c9ea60541dc6a2d850
wilmer <est.wzelayam@uml.edu.ni>
2023-08-12 21:26:22 -0600

    first commit

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -7,7 +7,7 @@ export const Title = (props: Props) => {
 
   return (
     <>
-    <div className='p-4 rounded-lg bg-rose-100'>
+    <div className='p-4 rounded-lg bg-rose-200'>
     <div className='px-10 text-[36px] font-bold text-center text-transparent uppercase bg-clip-text bg-gradient-to-b from-pink-400 to-pink-800'>{titulo}</div>
     </div>
     </>
